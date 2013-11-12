725b536934e99bee93fe1ed5f7613e24ffa80798
Jon Haus <haus.xda@gmail.com>
2013-11-12 17:36:34 -0500

    vold: Increase MAX_PARTITIONS to 48
    This is needed for certain devices with more than thirty two partitions. (eg. htc jewel, ville)
    
    Change-Id: I244f48c576af05b6569c8c40ebeb4ed1bfa53469

--- DirectVolume.h
+++ DirectVolume.h
@@ -27,7 +27,7 @@ typedef android::List<char *> PathCollection;
 
 class DirectVolume : public Volume {
 public:
-    static const int MAX_PARTITIONS = 32;
+    static const int MAX_PARTITIONS = 48;
 protected:
     const char* mMountpoint;
     const char* mFuseMountpoint;
