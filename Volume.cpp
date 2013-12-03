7442c4f55835dad9a7634e3ddf19871f0acb7954
Steve Kondik <shade@chemlab.org>
2013-12-03 23:41:23 +0530

    [2/3]vold: exFAT support
    smdk4412: https://gerrit.omnirom.org/#/c/3044/
    smdk4412-common: https://gerrit.omnirom.org/#/c/3132/
    
    Change-Id: I0606c45df874a8276068eb01c81f6d7e8235c1d7

--- Volume.cpp
+++ Volume.cpp
@@ -47,6 +47,7 @@
 #include "ResponseCode.h"
 #include "Ext4.h"
 #include "Fat.h"
+#include "Exfat.h"
 #include "Process.h"
 #include "cryptfs.h"
 
@@ -219,6 +220,8 @@ int Volume::createDeviceNode(const char *path, int major, int minor) {
 
 int Volume::formatVol(bool wipe) {
 
+    char* fstype = NULL;
+
     if (getState() == Volume::State_NoMedia) {
         errno = ENODEV;
         return -1;
@@ -270,7 +273,12 @@ int Volume::formatVol(bool wipe) {
         SLOGI("Formatting volume %s (%s)", getLabel(), devicePath);
     }
 
-    if (Fat::format(devicePath, 0, wipe)) {
+    fstype = getFsType((const char*)devicePath);
+    if (strcmp(fstype, "exfat") == 0) {
+        if (Exfat::format(devicePath)) {
+            SLOGE("Failed for format (%s) as exfat", strerror(errno));
+        }
+    } else if (Fat::format(devicePath, 0, wipe)) {
         SLOGE("Failed to format (%s)", strerror(errno));
         goto err;
     }
@@ -456,6 +464,23 @@ int Volume::mountVol() {
 
                 if (Ext4::doMount(devicePath, getMountpoint(), false, false, false, true)) {
                     SLOGE("%s failed to mount via EXT4 (%s)\n", devicePath, strerror(errno));
+                    continue;
+                }
+
+           } else if (strcmp(fstype, "exfat") == 0) {
+
+                if (Exfat::check(devicePath)) {
+                    errno = EIO;
+                    /* Badness - abort the mount */
+                    SLOGE("%s failed FS checks (%s)", devicePath, strerror(errno));
+                    setState(Volume::State_Idle);
+                    free(fstype);
+                    return -1;
+                }
+
+                if (Exfat::doMount(devicePath, "/mnt/secure/staging", false, false, false,
+                        AID_SYSTEM, gid, 0702)) {
+                    SLOGE("%s failed to mount via EXFAT (%s)\n", devicePath, strerror(errno));
                     continue;
                 }
 
