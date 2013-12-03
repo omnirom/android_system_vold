40a11010b57727434a20f05a722642ecd43a0bd0
UtkarshGupta <utkarsh.eminem@gmail.com>
2013-12-03 23:41:23 +0530

    vold: exFAT support
    smdk4412: https://gerrit.omnirom.org/#/c/3044/
    smdk4412-common: https://gerrit.omnirom.org/#/c/3132/
    
    Change-Id: I0606c45df874a8276068eb01c81f6d7e8235c1d7

--- Volume.cpp
+++ Volume.cpp
@@ -47,6 +47,7 @@
 #include "ResponseCode.h"
 #include "Ext4.h"
 #include "Fat.h"
+#include "Exfat.h"
 #include "Process.h"
 #include "cryptfs.h"
 
@@ -456,6 +457,23 @@ int Volume::mountVol() {
 
                 if (Ext4::doMount(devicePath, getMountpoint(), false, false, false, true)) {
                     SLOGE("%s failed to mount via EXT4 (%s)\n", devicePath, strerror(errno));
+                    continue;
+                }
+
+           } else if (strcmp(fstype, "exfat") == 0) {
+
+                if (Exfat::check(devicePath)) {
+                    errno = EIO;
+                    /* Badness - abort the mount */
+                    SLOGE("%s failed FS checks (%s)", devicePath, strerror(errno));
+                    setState(Volume::State_Idle);
+                    free(fstype);
+                    return -1;
+                }
+
+                if (Exfat::doMount(devicePath, "/mnt/secure/staging", false, false, false,
+                        AID_SYSTEM, gid, 0702)) {
+                    SLOGE("%s failed to mount via EXFAT (%s)\n", devicePath, strerror(errno));
                     continue;
                 }
 
