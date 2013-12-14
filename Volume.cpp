dd30d8519b40df18371e01b2f9b6e76770fdd991
Pawit Pornkitprasan <p.pawit@gmail.com>
2013-12-14 20:19:08 +0700

    vold [1/3]: don't crash when formatting unformatted volume
    When formatting unformatting volumes, fstype will be NULL causing
    a crash in strcmp. Set it to "vfat" to avoid the crash.
    
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Change-Id: I3876ea2a6d91e78f579537fd6ea3d030fb18ee7b

--- Volume.cpp
+++ Volume.cpp
@@ -249,8 +249,9 @@ int Volume::createDeviceNode(const char *path, int major, int minor) {
 }
 
 int Volume::formatVol(bool wipe) {
+
     const char* fstype = NULL;
-    
+
     if (getState() == Volume::State_NoMedia) {
         errno = ENODEV;
         return -1;
@@ -303,7 +304,7 @@ int Volume::formatVol(bool wipe) {
     if (mDebug) {
         SLOGI("Formatting volume %s (%s)", getLabel(), devicePath);
     }
-    
+
     fstype = getFsType((const char*)devicePath);
     if (fstype == NULL) {
         // Default to vfat
