f98ff4d0e37da6c619e657e5b4b132ac9f6727cf
Humberto Borba <humberos@gmail.com>
2014-12-15 17:09:07 -0200

    vold: use common flags
    just don't repeat yourself
    also, fix code style
    
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Change-Id: I32940d14b6dd0d4d24651397d55d640f28fa98b4

--- Android.mk
+++ Android.mk
@@ -1,5 +1,25 @@
 LOCAL_PATH:= $(call my-dir)
 
+common_cflags := \
+	-Werror=format \
+	-Wno-unused-parameter
+
+ifneq ($(BOARD_VOLD_MAX_PARTITIONS),)
+	common_cflags += -DVOLD_MAX_PARTITIONS=$(BOARD_VOLD_MAX_PARTITIONS)
+endif
+
+ifeq ($(BOARD_VOLD_EMMC_SHARES_DEV_MAJOR), true)
+	common_cflags += -DVOLD_EMMC_SHARES_DEV_MAJOR
+endif
+
+ifeq ($(BOARD_VOLD_DISC_HAS_MULTIPLE_MAJORS), true)
+	common_cflags += -DVOLD_DISC_HAS_MULTIPLE_MAJORS
+endif
+
+ifneq ($(TARGET_USE_CUSTOM_LUN_FILE_PATH),)
+	common_cflags += -DCUSTOM_LUN_FILE=\"$(TARGET_USE_CUSTOM_LUN_FILE_PATH)\"
+endif
+
 common_src_files := \
 	VolumeManager.cpp \
 	CommandListener.cpp \
@@ -58,39 +78,16 @@ common_static_libraries := \
 	libbatteryservice
 
 include $(CLEAR_VARS)
-
-ifeq ($(BOARD_VOLD_EMMC_SHARES_DEV_MAJOR), true)
-LOCAL_CFLAGS += -DVOLD_EMMC_SHARES_DEV_MAJOR
-endif
-
-ifeq ($(BOARD_VOLD_DISC_HAS_MULTIPLE_MAJORS), true)
-LOCAL_CFLAGS += -DVOLD_DISC_HAS_MULTIPLE_MAJORS
-endif
-
-ifneq ($(BOARD_VOLD_MAX_PARTITIONS),)
-LOCAL_CFLAGS += -DVOLD_MAX_PARTITIONS=$(BOARD_VOLD_MAX_PARTITIONS)
-endif
-
-ifneq ($(TARGET_USE_CUSTOM_LUN_FILE_PATH),)
-LOCAL_CFLAGS += -DCUSTOM_LUN_FILE=\"$(TARGET_USE_CUSTOM_LUN_FILE_PATH)\"
-endif
-
 LOCAL_MODULE := libvold
-
 LOCAL_SRC_FILES := $(common_src_files)
-
 LOCAL_C_INCLUDES := $(common_c_includes)
-
 LOCAL_SHARED_LIBRARIES := $(common_shared_libraries)
-
 LOCAL_STATIC_LIBRARIES := $(common_static_libraries)
-
+LOCAL_CFLAGS := $(common_cflags)
 LOCAL_MODULE_TAGS := eng tests
-
 include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-
 LOCAL_MODULE:= vold
 
 LOCAL_SRC_FILES := \
@@ -98,41 +95,15 @@ LOCAL_SRC_FILES := \
 	$(common_src_files)
 
 LOCAL_C_INCLUDES := $(common_c_includes)
-
-LOCAL_CFLAGS := -Werror=format
-
-ifeq ($(BOARD_VOLD_EMMC_SHARES_DEV_MAJOR), true)
-LOCAL_CFLAGS += -DVOLD_EMMC_SHARES_DEV_MAJOR
-endif
-
-ifeq ($(BOARD_VOLD_DISC_HAS_MULTIPLE_MAJORS), true)
-LOCAL_CFLAGS += -DVOLD_DISC_HAS_MULTIPLE_MAJORS
-endif
-
-ifneq ($(BOARD_VOLD_MAX_PARTITIONS),)
-LOCAL_CFLAGS += -DVOLD_MAX_PARTITIONS=$(BOARD_VOLD_MAX_PARTITIONS)
-endif
-
-ifneq ($(TARGET_USE_CUSTOM_LUN_FILE_PATH),)
-LOCAL_CFLAGS += -DCUSTOM_LUN_FILE=\"$(TARGET_USE_CUSTOM_LUN_FILE_PATH)\"
-endif
-
+LOCAL_CFLAGS := $(common_cflags)
 LOCAL_SHARED_LIBRARIES := $(common_shared_libraries)
-
 LOCAL_STATIC_LIBRARIES := $(common_static_libraries)
-
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
-
 LOCAL_SRC_FILES:= vdc.c
-
 LOCAL_MODULE:= vdc
-
 LOCAL_C_INCLUDES :=
-
 LOCAL_CFLAGS := 
-
 LOCAL_SHARED_LIBRARIES := libcutils
-
 include $(BUILD_EXECUTABLE)
