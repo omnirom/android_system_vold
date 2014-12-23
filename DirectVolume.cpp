1197041e74e7aa859693e9f3930c044e9c5c0f8c
Jiangyi <sam.andrew.jiang@gmail.com>
2014-12-23 17:59:06 -0500

    Vold: Don't reset mPartMinors array when handling disk
    The mPartMinors array is already initialized as -1 earlier, there is no point in setting
    it again in the process of handling disk added/changed.
    
    Also, on some Samsung devices, certain block devices report themselves to be disks, when
    they are in fact not. These block devices cause the handleDiskAdded function to be run again
    for no reason, as these "disks" are partition-less. By doing so, it causes the mPartMinors
    array to be reset back to -1, leading to an inability to mount partitions that were part of
    the original array, as they now have no minor number to reference to.
    
    Change-Id: I78f9bb3914165fb6b247e0e5fc39c543bd1e596d

--- DirectVolume.cpp
+++ DirectVolume.cpp
@@ -211,8 +211,6 @@ void DirectVolume::handleDiskAdded(const char * /*devpath*/,
     }
 
     mPendingPartCount = mDiskNumParts;
-    for (int i = 0; i < MAX_PARTITIONS; i++)
-        mPartMinors[i] = -1;
 
     if (mDiskNumParts == 0) {
 #ifdef PARTITION_DEBUG
@@ -312,8 +310,6 @@ void DirectVolume::handleDiskChanged(const char * /*devpath*/,
     }
 
     mPendingPartCount = mDiskNumParts;
-    for (int i = 0; i < MAX_PARTITIONS; i++)
-        mPartMinors[i] = -1;
 
     if (getState() != Volume::State_Formatting) {
         if (mDiskNumParts == 0) {
