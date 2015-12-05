eb730b5c2e4bd4a0348a4f379afdad07007d61a0
Ketut Putu Kumajaya <ketut.kumajaya@gmail.com>
2015-12-05 19:04:21 +0700

    vold: Fix native exfat driver support
    Change-Id: I772368c0ec054a2ba86cf0ba0e37f07ba2b82fd2

--- Android.mk
+++ Android.mk
@@ -85,6 +85,10 @@ ifeq ($(TARGET_USERIMAGES_USE_EXT4), true)
   endif
 endif
 
+ifeq ($(TARGET_KERNEL_HAVE_EXFAT),true)
+vold_cflags += -DCONFIG_KERNEL_HAVE_EXFAT
+endif
+
 include $(CLEAR_VARS)
 
 LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/Android.mk
@@ -115,10 +119,6 @@ LOCAL_INIT_RC := vold.rc
 LOCAL_C_INCLUDES := $(common_c_includes)
 LOCAL_CFLAGS := $(vold_cflags)
 LOCAL_CONLYFLAGS := $(vold_conlyflags)
-
-ifeq ($(TARGET_KERNEL_HAVE_EXFAT),true)
-LOCAL_CFLAGS += -DCONFIG_KERNEL_HAVE_EXFAT
-endif
 
 LOCAL_SHARED_LIBRARIES := $(common_shared_libraries)
 LOCAL_STATIC_LIBRARIES := $(common_static_libraries)
