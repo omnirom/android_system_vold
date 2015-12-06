3b1bbcb5e3c75c090baf689c95b2439591872463
dhacker29 <dhackerdvm@gmail.com>
2015-12-06 05:32:30 -0500

    vold: Native NTFS kernel driver support
    Change-Id: I825b2840a03e5cdab9b68f13fd5170acbbcec2e5

--- Android.mk
+++ Android.mk
@@ -108,6 +108,10 @@ ifeq ($(TARGET_KERNEL_HAVE_EXFAT),true)
 vold_cflags += -DCONFIG_KERNEL_HAVE_EXFAT
 endif
 
+ifeq ($(TARGET_KERNEL_HAVE_NTFS),true)
+vold_cflags += -DCONFIG_KERNEL_HAVE_NTFS
+endif
+
 include $(CLEAR_VARS)
 
 LOCAL_ADDITIONAL_DEPENDENCIES := $(LOCAL_PATH)/Android.mk
