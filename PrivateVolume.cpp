cf6ae02fd41758cc2c59db2db4d0f5b1aad023e4
Michael Bestas <mikeioannina@cyanogenmod.org>
2016-01-20 22:06:11 +0200

    vold: Honor mount options for f2fs partitions
    * Based on 1436fc7ea3b349283a021e75fdd4f3478a2ca2e8
    
    Change-Id: Id9491ada3bd34275a95ef9855167db6ae7a9b30f

--- PrivateVolume.cpp
+++ PrivateVolume.cpp
@@ -128,7 +128,7 @@ status_t PrivateVolume::doMount() {
             return -EIO;
         }
 
-        if (f2fs::Mount(mDmDevPath, mPath, true)) {
+        if (f2fs::Mount(mDmDevPath, mPath, "", true)) {
             PLOG(ERROR) << getId() << " failed to mount";
             return -EIO;
         }
