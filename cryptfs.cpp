58d5754abd2b427f1358dbe6a66e4222174843ca
Marko Man <darkobas@gmail.com>
2019-07-24 14:09:57 +0200

    Revert "system: vold: FDE target boot"
    This reverts commit 81012e2347fc014a74c76b9bb653e093edd7ef8f.
    
    Change-Id: I2ded8a393865fe14c2c773d20e6be727be779454

--- cryptfs.cpp
+++ cryptfs.cpp
@@ -1238,7 +1238,6 @@ static int get_dm_crypt_version(int fd, const char *name,  int *version)
     return -1;
 }
 
-#ifndef CONFIG_HW_DISK_ENCRYPTION
 static std::string extra_params_as_string(const std::vector<std::string>& extra_params_vec) {
     if (extra_params_vec.empty()) return "";
     std::string extra_params = std::to_string(extra_params_vec.size());
@@ -1248,10 +1247,8 @@ static std::string extra_params_as_string(const std::vector<std::string>& extra_
     }
     return extra_params;
 }
-#endif
 
 // Only adds parameters if the property is set.
-#ifndef CONFIG_HW_DISK_ENCRYPTION
 static void add_sector_size_param(std::vector<std::string>* extra_params_vec) {
     constexpr char DM_CRYPT_SECTOR_SIZE[] = "ro.crypto.fde_sector_size";
     char sector_size[PROPERTY_VALUE_MAX];
@@ -1265,7 +1262,6 @@ static void add_sector_size_param(std::vector<std::string>* extra_params_vec) {
         extra_params_vec->emplace_back("iv_large_sectors");
     }
 }
-#endif
 
 static int create_crypto_blk_dev(struct crypt_mnt_ftr* crypt_ftr, const unsigned char* master_key,
                                  const char* real_blk_name, char* crypto_blk_name, const char* name,
@@ -1343,8 +1339,6 @@ static int create_crypto_blk_dev(struct crypt_mnt_ftr* crypt_ftr, const unsigned
         }
       }
     }
-    load_count = load_crypto_mapping_table(crypt_ftr, master_key, real_blk_name, name, fd,
-                                           extra_params);
 #else
     if (!get_dm_crypt_version(fd, name, version)) {
         /* Support for allow_discards was added in version 1.11.0 */
@@ -1355,10 +1349,10 @@ static int create_crypto_blk_dev(struct crypt_mnt_ftr* crypt_ftr, const unsigned
     if (flags & CREATE_CRYPTO_BLK_DEV_FLAGS_ALLOW_ENCRYPT_OVERRIDE) {
         extra_params_vec.emplace_back("allow_encrypt_override");
     }
+#endif
     add_sector_size_param(&extra_params_vec);
     load_count = load_crypto_mapping_table(crypt_ftr, master_key, real_blk_name, name, fd,
                                            extra_params_as_string(extra_params_vec).c_str());
-#endif
     if (load_count < 0) {
         SLOGE("Cannot load dm-crypt mapping table.\n");
         goto errout;
